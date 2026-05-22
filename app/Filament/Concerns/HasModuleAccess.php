e15bcbd8548074693d124cd99fc0b56dc5dbcb7f
Damell Bell <58995862+damell01@users.noreply.github.com>
2026-05-22 08:06:20 -0500

    Update

--- app/Filament/Concerns/HasModuleAccess.php
+++ app/Filament/Concerns/HasModuleAccess.php
@@ -6,8 +6,6 @@ use App\Support\AdminModules;
 
 trait HasModuleAccess
 {
-    protected static string $moduleSlug;
-
     public static function canAccess(): bool
     {
         return AdminModules::isEnabled(static::$moduleSlug) && static::passesModuleAccessCheck();

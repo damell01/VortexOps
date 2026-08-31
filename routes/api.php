f714bac637620ed92fb824c7dfa595b9de36ad80
Damell Bell <58995862+damell01@users.noreply.github.com>
2026-08-31 18:12:27 -0500

    feat(whatnot): expose desktop collector endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ShowImportController;
+use App\Http\Controllers\Api\WhatnotCollectorController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api.token', 'throttle:60,1'])->group(function () {
@@ -8,4 +9,10 @@ Route::middleware(['api.token', 'throttle:60,1'])->group(function () {
     Route::get('/shows/{show}', [ShowImportController::class, 'show']);
     Route::get('/channels', [ShowImportController::class, 'channels']);
     Route::get('/streamers', [ShowImportController::class, 'streamers']);
+
+    Route::prefix('whatnot/collector')->group(function () {
+        Route::get('/bootstrap', [WhatnotCollectorController::class, 'bootstrap']);
+        Route::post('/import', [WhatnotCollectorController::class, 'import']);
+        Route::get('/latest', [WhatnotCollectorController::class, 'latest']);
+    });
 });

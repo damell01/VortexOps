53509ce06b43f6f08a15921e95e425d64953ca3f
Damell Bell <58995862+damell01@users.noreply.github.com>
2026-09-08 10:46:03 -0500

    Send streamers straight to My Shows after login

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages\Auth;
 
+use App\Filament\Pages\StreamerShows;
 use Filament\Auth\Pages\Login as BaseLogin;
 use Filament\Forms\Components\Checkbox;
 use Filament\Schemas\Components\Component;
@@ -12,5 +13,16 @@ class Login extends BaseLogin
     {
         return Checkbox::make('remember')
             ->label('Remember me for 30 days');
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        $user = auth()->user();
+
+        if ($user?->isStreamer() && ! $user->isAdmin() && ! $user->isOwner()) {
+            return StreamerShows::getUrl();
+        }
+
+        return parent::getRedirectUrl();
     }
 }
